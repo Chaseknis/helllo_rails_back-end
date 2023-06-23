aa0edb2b4fa49cd395a0fc65d7aa68e1c815d58b
chaseknis <chase6th@gmail.com>
2023-06-23 21:01:31 +0200

    Add linters to the project Update routes.rb, schema.rb and seeds.rb Create messages_controller.rb and add a migration file

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,12 @@
+class Api::MessagesController < ApplicationController
+    def random_greeting
+      message = Message.order('RANDOM()').first
+  
+      if message.nil?
+        render json: { error: 'No messages found' }, status: :not_found
+      else
+        render json: { greeting: message.text }
+      end
+    end
+  end
+  

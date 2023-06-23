b8d015ed6d284f63d7d67b7f0896fa888776cdfc
chaseknis <chase6th@gmail.com>
2023-06-23 21:23:17 +0200

    Fix linter errors

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,12 +1,11 @@
 class Api::MessagesController < ApplicationController
-    def random_greeting
-      message = Message.order('RANDOM()').first
-  
-      if message.nil?
-        render json: { error: 'No messages found' }, status: :not_found
-      else
-        render json: { greeting: message.text }
-      end
+  def random_greeting
+    message = Message.order('RANDOM()').first
+
+    if message.nil?
+      render json: { error: 'No messages found' }, status: :not_found
+    else
+      render json: { greeting: message.text }
     end
   end
-  
+end
